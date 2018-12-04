b976c2ac19b9fe09c1e8507756255ff8c07a1a37
Ian Sime <Iansime@Vickis-MacBook-Pro-2.local>
2018-12-03 21:06:50 -0700

    First push of POC

---
+++ SkiClock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkiClock
+//
+//  Created by Ian Sime on 12/2/18.
+//  Copyright © 2018 Ian Sime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
